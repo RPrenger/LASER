a1f53ffb30a78d8cd3d1d42a4fe35b4cb5270db8
rprenger <rprenger@nvidia.com>
2021-01-13 19:09:15 +0000

    Changing fix_files to be more correct and deal with corrupt files

--- tasks/CCMatrix/fix_files.py
+++ tasks/CCMatrix/fix_files.py
@@ -8,20 +8,27 @@ def fix(filename):
     f = gzip.open(filename, 'rt')
     o = gzip.open("/datadrive/tmp.tsv.gz", 'wt')
     final_line = ""
-    for i, line in enumerate(f):
-        if line[-1] == '\n':
-            line = line[:-1]
-        z = re.match("^[a-z][a-z]-[a-z][a-z]/[a-z][a-z]", line)
-        if not z:
-            final_line = final_line + line
-        else:
-            if i > 0:
-                o.write(final_line+'\n')
-            final_line = line
-    o.write(final_line+'\n')
-    f.close()
-    o.close()
-    copyfile("/datadrive/tmp.tsv.gz", filename)
+    try:
+        for i, line in enumerate(f):
+            if line[-1] == '\n':
+                line = line[:-1]
+            z = re.match("^[a-z][a-z]-[a-z][a-z]/[a-z][a-z]", line)
+            if not z:
+                assert(i > 0)
+                final_line = final_line + line
+            else:
+                if i > 0:
+                    o.write(final_line+'\n')
+                final_line = line
+        assert(len(final_line) > 4)
+        o.write(final_line+'\n')
+        f.close()
+        o.close()
+        copyfile("/datadrive/tmp.tsv.gz", filename)
+    except:
+        f.close()
+        o.close()
+        os.remove(filename)
     os.remove("/datadrive/tmp.tsv.gz")
 
 if __name__ == "__main__":
